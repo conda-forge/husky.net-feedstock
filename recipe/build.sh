800d6ff82537a42e644e46ddd4958fd37d160a0a
Daniel Nachun <daniel.nachun@gmail.com>
2025-04-08 21:19:02 -0700

    update to 0.7.2

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,21 +5,24 @@ set -o xtrace -o nounset -o pipefail -o errexit
 mkdir -p ${PREFIX}/bin
 mkdir -p ${PREFIX}/libexec/${PKG_NAME}
 ln -sf ${DOTNET_ROOT}/dotnet ${PREFIX}/bin
+ln -sf ${PREFIX}/bin/Husky ${PREFIX}/bin/dotnet-husky
 
 # Build package with dotnet publish
 rm -rf global.json
+rm -rf .config/dotnet-tools.json
 framework_version="$(dotnet --version | sed -e 's/\..*//g').0"
-dotnet publish --no-self-contained src/Husky/Husky.csproj --output ${PREFIX}/libexec/${PKG_NAME} --framework "net${framework_version}"
+dotnet publish --no-self-contained src/Husky/Husky.csproj --output ${PREFIX}/libexec/${PKG_NAME} --framework net${framework_version}
 rm ${PREFIX}/libexec/${PKG_NAME}/Husky
+rm ${PREFIX}/bin/dotnet-husky
 
 # Create bash and batch wrappers
-tee ${PREFIX}/bin/Husky << EOF
+tee ${PREFIX}/bin/dotnet-husky << EOF
 #!/bin/sh
 exec \${DOTNET_ROOT}/dotnet exec \${CONDA_PREFIX}/libexec/husky.net/Husky.dll "\$@"
 EOF
-chmod +x ${PREFIX}/bin/Husky
+chmod +x ${PREFIX}/bin/dotnet-husky
 
-tee ${PREFIX}/bin/Husky.cmd << EOF
+tee ${PREFIX}/bin/dotnet-husky.cmd << EOF
 call %DOTNET_ROOT%\dotnet exec %CONDA_PREFIX%\libexec\husky.net\Husky.dll %*
 EOF
 
